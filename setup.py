95eec715e6c770ca7990be2607f713f6a153d2a2
Richard D. Paul <richard@los-paul.eu>
2022-02-02 14:49:41 +0100

    inc version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='docov',
-    version='0.0.2',
+    version='0.0.3',
     description='Light-weight docstring coverage analysis for python modules', 
     long_description=long_description, 
     long_description_content_type='text/markdown', 
